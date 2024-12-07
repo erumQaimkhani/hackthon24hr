5244780b27803d33cf378909ee8322a4452f807b
erumQaimkhani <kirannaveed523@gmail.com>
2024-12-07 23:35:12 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Home</h1>
+      <img src="https://picsum.photos/200/300" height={300}width={200} />
+    </div>
+  );
+}
